484ff6129320a61c82ffbb06dbf3f3a49f68512a
Elison Liu <elisonliu97@gmail.com>
2021-07-25 19:57:20 -0400

    contact me page stlying

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -7,18 +7,18 @@ function NavBar(props) {
         // navbar portion
         <nav>
             <ul>
-                <li onClick={() => props.handlePageChange("About")}>
+                <a href="#"><li onClick={() => props.handlePageChange("About")}>
                     About
-                </li>
-                <li onClick={() => props.handlePageChange("Portfolio")}>
+                </li></a>
+                <a href="#"><li onClick={() => props.handlePageChange("Portfolio")}>
                     Portfolio
-                </li>
-                <li onClick={() => props.handlePageChange("Contact")}>
+                </li></a>
+                <a href="#"><li onClick={() => props.handlePageChange("Contact")}>
                     Contact
-                </li>
-                <li onClick={() => props.handlePageChange("Resume")}>
-                    <a href="https://drive.google.com/file/d/17GR5d24QC7zzlLlR6FvQdRZf9NnWx4Id/view">Resume</a>
-                </li>
+                </li></a>
+                <a href="https://drive.google.com/file/d/17GR5d24QC7zzlLlR6FvQdRZf9NnWx4Id/view"><li onClick={() => props.handlePageChange("Resume")}>
+                    Resume
+                </li></a>
 
             </ul>
         </nav >
--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -2,15 +2,58 @@ import React, { useState } from 'react';
 import './contact.css'
 
 function Contact() {
-    const [contactForm, setContactForm] = useState({
-        name:"",
-        email:"",
-        subject:"",
-    });
+    const emailValidator = (email) => {
+        // Regex to validate an email by RFC2822
+        const emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/ 
+        return emailRegex.test(email.toLowerCase());
+    }
 
-    // need 
-    const submitHandler = () => {
-        return
+    // need submitHandler function    
+    const submitHandler = (event) => {
+        event.preventDefault();
+        // element selectors
+        const nameEl = document.querySelector("#input-name").value.trim();
+        const emailEl = document.querySelector("#input-email").value.trim();
+        const subjectEl = document.querySelector("#input-subject").value.trim();
+
+        if (!nameEl || !emailEl || !subjectEl){
+            if(!nameEl) {
+                document.querySelector("#label-name").innerHTML = "Name: <span style='color:red;'>NAME IS REQUIRED</span>"
+            } else {
+                document.querySelector("#label-name").innerHTML = "Name:"
+            }
+            if(!emailEl) {
+                document.querySelector('#label-email').innerHTML = "Email: <span style='color:red;'>EMAIL IS REQUIRED</span>"
+            } else {
+                document.querySelector('#label-email').innerHTML = "Email:"
+            }
+
+            if(!subjectEl) {
+                document.querySelector("#label-subject").innerHTML = "Subject: <span style='color:red;'>SUBJECT IS REQUIRED</span>"
+            } else {
+                document.querySelector("#label-subject").innerHTML = "Subject:"
+            }
+            return
+        }
+
+        document.querySelector("#label-name").innerHTML = "Name:"
+        document.querySelector('#label-email').innerHTML = "Email:"
+        document.querySelector("#label-subject").innerHTML = "Subject:"
+
+        if (!emailValidator(emailEl)){
+            document.querySelector('#label-email').innerHTML = "Email: <span style='color:red;'>INVALID EMAIL</span>"
+            return
+        } else {
+            document.querySelector('#label-email').innerHTML = "Email:"
+        }
+
+        window.alert("MESSAGE SENT")
+
+        document.querySelector("#input-name").value = ""
+        document.querySelector("#input-email").value = ""
+        document.querySelector("#input-subject").value = ""
+
+        return 
     }
 
 
@@ -18,22 +61,20 @@ function Contact() {
         <div class="contact-container">
             <h2>Contact Me</h2>
             {/* submit handler might need to be on button */}
-            <form class="form-container" onSubmit={() => submitHandler()}>
+            <form class="form-container"  onSubmit={(event) => submitHandler(event)}>
                 <div class="input-container">
-                    <label>Name</label>
-                    <textarea id="input-name"></textarea>
+                    <label id="label-name">Name:</label>
+                    <input id="input-name"></input>
                 </div>
                 <div class="input-container">
-                    <label>Email</label>
-                    <textarea id="input-email"></textarea>
+                    <label id="label-email">Email:</label>
+                    <input id="input-email"></input>
                 </div>
                 <div class="input-container">
-                    <label>Subject</label>
+                    <label id="label-subject">Subject:</label>
                     <textarea id="input-subject"></textarea>
                 </div>
-                <div class="input-container">
                     <button>Submit</button>
-                </div>
             </form>
         </div>
     )
